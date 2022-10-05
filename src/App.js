e900da1c60e3c7736e2af6be4dc90ccde2c53f9e
vino00619 <vinoth00619@gmail.com>
2022-10-05 23:07:05 +0530

    resume link updated

--- src/App.js
+++ src/App.js
@@ -167,7 +167,7 @@ function App() {
             </button>
           </a>
           <a
-            href="https://drive.google.com/file/d/1qq3DprArXpHyDaovwdbQ5LYG1ffousXZ/view?usp=sharing"
+            href="https://drive.google.com/file/d/1bpa6GodO2_o4XsgR3TvPp14QBaLI4l05/view?usp=sharing"
             target="_blank"
             rel="noreferrer"
           >
